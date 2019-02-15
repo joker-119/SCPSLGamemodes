41a9325bc161b2a9c65c6bddb77ef08fd9db7c89
Josh Raymond <amathor929@gmail.com>
2019-02-14 23:58:16 -0800

    Fixed(?) Blackout not enabling on Survival round starts.

--- SurvivalOfTheFittestGamemode/EventsHandler.cs
+++ SurvivalOfTheFittestGamemode/EventsHandler.cs
@@ -265,14 +265,7 @@ namespace SurvivalGamemode
         public void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
         {
             plugin.Info("Timer completed!");
-            foreach (Smod2.Plugin p in PluginManager.Manager.EnabledPlugins)
-            {
-                if (p.Details.id == "Blackout" && p is Blackout.Plugin)
-                {
-                    plugin.Info("Toggling Blackout on.");
-                    Blackout.Plugin.ToggleBlackout();
-                }
-            }
+            Blackout.Plugin.ToggleBlackout();
             Vector spawn = plugin.Server.Map.GetRandomSpawnPoint(Role.SCP_939_53);
             foreach (Player player in plugin.Server.GetPlayers())
             {
